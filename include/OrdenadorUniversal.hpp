afc8a28d59246dba371defd88a05051fb8ed8301
gabriel <gabrielcarlosj@gmail.com>
2025-05-01 17:21:28 -0300

    Trying: breaking threshold

--- include/OrdenadorUniversal.hpp
+++ include/OrdenadorUniversal.hpp
@@ -3,20 +3,36 @@
 
 class OrdenadorUniversal
 {
+    struct Estatisticas{
+        unsigned cmp, move, calls, mps;
+        long double valorCusto;
+    };
+    
+
     private:
-        unsigned cmp;
-        unsigned move;
-        unsigned calls;
+        unsigned cmp, move, calls;
+        double a, b, c;
+        Estatisticas *custos;
+        
     public:
-        OrdenadorUniversal();
+        OrdenadorUniversal(int tam);
         ~OrdenadorUniversal();  
     
-        void ordenador(int *V, int tam, int minTamParticao, int limiarQuebras, int numeroQuebras);
+        // limiarCusto
+        int determinaLimiarQuebras(int *V, int tam, int limiarCusto);
+
+        // limiarParticao
+        void ordenador(int *V, int tam, int minTamParticao, int limiarQuebras);
         int determinaLimiarParticao(int *V, int tam, int limiarCusto);
-        void calculaNovaFaixa(int limParticao, int minMPS, int maxMPS, int passoMPS);
+        void calculaNovaFaixa(int limParticao, int minMPS, int maxMPS, int passoMPS, int numMPS);
+        void registraEstatisticas(int numMPS, unsigned t);
+        void imprimeEstatisticas(int numMPS);
 
         // Auxiliary methods
+        int numeroQuebras(int *V, int tam);
+        int menorCusto(int tam);
         void resetStats();
+        void resetCustos();
         void swap(int &a, int &b);
 
         // Quicksort methods
--- src/OrdenadorUniversal.cpp
+++ src/OrdenadorUniversal.cpp
@@ -1,50 +1,172 @@
 #include "./../include/OrdenadorUniversal.hpp"
 
-OrdenadorUniversal::OrdenadorUniversal(){
+#include <iostream>
+
+using namespace std;
+
+OrdenadorUniversal::OrdenadorUniversal(int tam){
     resetStats();
+    custos = new Estatisticas[6];
 }
 
 OrdenadorUniversal::~OrdenadorUniversal(){}
 
 
-void OrdenadorUniversal::ordenador(int *V, int tam, int minTamParticao, int limiarQuebras, int numeroQuebras) {
-    
-    if (numeroQuebras < limiarQuebras) 
-        insertionSort(V, 0, tam);   
+void OrdenadorUniversal::ordenador(int *V, int tam, int minTamParticao, int limiarQuebras) {
+    int numQuebras = numeroQuebras(V, tam);
+
+    if (numQuebras < limiarQuebras) 
+        insertionSort(V, 0, tam-1);   
     else if (tam > minTamParticao) 
-            quickSort3Ins(V, 0, tam) ;
+        quickSort3Ins(V, 0, tam-1) ;
     else 
-        insertionSort(V, 0, tam);
+        insertionSort(V, 0, tam-1);
 }
+
+int OrdenadorUniversal::determinaLimiarQuebras(int *V, int tam, int limiarCusto){
+    int limiarQuebras,
+        minQuebras = 0, 
+        maxQuebras = tam,
+        numQuebras,
+        index = 0,
+        passoQuebra = (maxQuebras-minQuebras)/5;
+    
+    long double diffCusto = 0;
+    
+    do {
+        numQuebras = 0;
+        cout << "iter " << index << endl;
+
+        for (int lq = minQuebras; lq <= maxQuebras; lq += passoQuebra) {
+            // QuickSort
+            ordenador(V, tam, 0, lq); // 'tam' como minTamParticao para forçar a escolha baseada no limiar de quebras
+            registraEstatisticas(numQuebras, lq); // Registra as estatísticas para este limiar de quebras
+            cout << "qs lq " << lq << " ";
+            imprimeEstatisticas(numQuebras);
+            resetStats();
+
+            // InsertionSort
+            ordenador(V, tam, tam+1, lq); // 'tam' como minTamParticao para forçar a escolha baseada no limiar de quebras
+            registraEstatisticas(numQuebras, lq); // Registra as estatísticas para este limiar de quebras
+            cout << "in lq " << lq << " ";
+            imprimeEstatisticas(numQuebras);
+            resetStats();
+
+
+            numQuebras++;
+        }
+
+        limiarQuebras = menorCusto(tam); // Encontra o limiar de quebras com menor custo registrado
+        calculaNovaFaixaQuebras(limiarQuebras, minQuebras, maxQuebras, passoQuebra, numQuebras);
+        diffCusto = (custos[minQuebras].valorCusto - custos[maxQuebras].valorCusto) < 0 ?
+                   -(custos[minQuebras].valorCusto - custos[maxQuebras].valorCusto) :
+                    (custos[minQuebras].valorCusto - custos[maxQuebras].valorCusto);
+        index++;
+        resetCustos();
+        cout << endl;
+    } while ((diffCusto > limiarCusto) && (numQuebras >= 5));
+
+    cout << "numq " << index - 1 << " limQuebras " << limiarQuebras << " qdiff " ; // ARRUMAR - o que é qdiff
+    return limiarQuebras;
+}
+
 
 
 int OrdenadorUniversal::determinaLimiarParticao(int *V, int tam, int limiarCusto) {
-    int minMPS = 2, numMPS, maxMPS = tam;
-    int passoMPS = (maxMPS - minMPS) / 5;
-    int diffCusto = 0;
-
+    int minMPS = 2, 
+        numMPS, 
+        maxMPS = tam,
+        passoMPS = (maxMPS - minMPS) / 5,
+        limParticao,
+        index = 0;
+    long double diffCusto = 0;
+        
     do {
         numMPS = 0;
+        cout << "iter " << index << endl;
         
         for (int t = minMPS; t <= maxMPS; t += passoMPS) {
             ordenador(V, tam, t, tam);
-            registraEstatisticas(custo[numMPS]);
-            imprimeEstatisticas(custo[numMPS]);
+            registraEstatisticas(numMPS, t);
+            cout << "mps " << t << " ";
+            imprimeEstatisticas(numMPS);
+            resetStats(); 
             numMPS++;
         }
         
-        limParticao = menorCusto();
-        calculaNovaFaixa(limParticao, minMPS, maxMPS, passoMPS);
-        diffCusto = fabs(custo[minMPS] - custo[maxMPS]);
-    } while ((diffCusto > limiarCusto) && (numMPS >= 5)) 
-    
+        limParticao = menorCusto(tam);
+        calculaNovaFaixa(limParticao, minMPS, maxMPS, passoMPS, numMPS);
+        diffCusto = (custos[minMPS].valorCusto - custos[maxMPS].valorCusto) < 0 ?
+                   -(custos[minMPS].valorCusto - custos[maxMPS].valorCusto) : 
+                    (custos[minMPS].valorCusto - custos[maxMPS].valorCusto);
+        index++;
+        resetCustos();
+        cout << endl;
+    } while ((diffCusto > limiarCusto) && (numMPS >= 5));
+    cout << "nummps " << index-1 << " limParticao " << limParticao << " mpsdiff " ; // ARRUMAR - oq é mpsdiff
     return limParticao;
 }
 
+void OrdenadorUniversal::calculaNovaFaixa(int limParticao, int minMPS, int maxMPS, int passoMPS, int numMPS){
+    int newMin, newMax;
+    
+    if (limParticao == 0) {
+        newMin = 0;
+        newMax = 2;
+    } else if (limParticao == numMPS - 1) {
+        newMin = numMPS - 3;
+        newMax = numMPS - 1;
+    } else {
+        newMin = limParticao - 1;
+        newMax = limParticao + 1;
+    }
+    
+      // getMPS(index) virou custos[index].mps
+    minMPS = custos[newMin].mps;
+    maxMPS = custos[newMax].mps;
+
+    passoMPS = (int)(maxMPS - minMPS) / 5;
+    
+    if (passoMPS == 0) {
+        passoMPS++;
+    }
+}
 
 
+int OrdenadorUniversal::menorCusto(int tam) {
+    int menorIdx = 0;
+    for (int i = 1; i < tam; i++) {
+        if (custos[i].valorCusto < custos[menorIdx].valorCusto)
+            menorIdx = i;
+    }
+    return menorIdx;
+}
 
+void OrdenadorUniversal::registraEstatisticas(int index, unsigned t){
+    custos[index].mps = t;
+    custos[index].calls = calls;
+    custos[index].cmp = cmp;
+    custos[index].move = move;
+    custos[index].valorCusto = a*cmp + b*move + c*calls;
+}
 
+void OrdenadorUniversal::imprimeEstatisticas(int index){
+    cout << "cost " << custos[index].valorCusto << " "
+         << "cmp " << custos[index].cmp <<  " "
+         << "move " << custos[index].move << " "
+         << "calls " << custos[index].calls << endl;
+}
+
+int OrdenadorUniversal::numeroQuebras(int *V, int tam) {
+    int quebras = 0;
+
+    for (int i = 1; i < tam; i++) {
+        cmp++; // será?
+        if (V[i] < V[i-1]) 
+            quebras++;
+    }
+    return quebras;
+}
 
 
 
@@ -54,11 +176,18 @@ void OrdenadorUniversal::resetStats(){
     calls = 0;
 }
 
+void OrdenadorUniversal::resetCustos(){
+    for (int i = 0; i < 6; i++)
+        custos[i].valorCusto = -1;
+}
+
+
 void OrdenadorUniversal::swap(int &a, int &b){
     int temp = a;
     a=b;
     b=temp;
     move+=3;
+    
 }
 
 
@@ -118,7 +247,7 @@ void OrdenadorUniversal::partition3(int * A, int l, int r, int *i, int *j){
 }
 
 // quicksort with insertion for small partitions and median of 3
-void OrdenadorUniversal::quickSort3Ins(int * A, int l, int r) { 
+void OrdenadorUniversal::quickSort3Ins(int * A, int l, int r) { // ARRUMAR - acho q é sem insertio, visto q o insertion é em outra func
     calls += 2;
 
     int i, j;
